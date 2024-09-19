8dbcea22a2ed14a3f3e5830cc3c1b2f2f6f23dcd
Guillermo Domínguez <widomin@hotmail.com>
2024-09-19 16:21:05 +0200

    ecosystem

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,19 +1,13 @@
 module.exports = {
-    apps: [
-        {
-            name: 'auth-2',
-            exec_mode: 'cluster',
-            instances: 1,
-            autorestart: true,
-            watch: true,
-            ignore_watch: ["node_modules", "dist", ".git"],
-            script: './dist/index.js',
-            env: {
-                ENV: 'development',
-            },
-            env_production: {
-                ENV: 'production',
-            }
-        }
-    ]
-};
+    apps : [{
+      name: "api",
+      script: "./dist/index.js",
+      env: {
+        NODE_ENV: "production",
+      },
+      exec_mode: "cluster",
+      instances: "max",
+      watch: false,
+      max_memory_restart: "1G",
+    }]
+  };
