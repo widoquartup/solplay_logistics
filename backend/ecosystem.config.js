c89567258c306eb352fa31942a0e18cacf5a5985
Guillermo Domínguez <widomin@hotmail.com>
2024-09-20 11:00:23 +0200

    backend ecosystem

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,13 +1,18 @@
 module.exports = {
-    apps : [{
-      name: "backend",
-      script: "./dist/index.js",
-      env: {
-        NODE_ENV: "production",
-      },
-      exec_mode: "fork",
-      instances: 1,
-      watch: false,
-      max_memory_restart: "1G",
-    }]
-  };
+  apps : [{
+    name: "backend",
+    script: "./dist/index.js",
+    cwd: "C:/Users/Almacen Lonas/app_almacen/solplay_logistics/backend",
+    env: {
+      NODE_ENV: "production",
+    },
+    exec_mode: "fork",
+    instances: 1,
+    watch: false,
+    max_memory_restart: "1G",
+    error_file: "logs/err.log",
+    out_file: "logs/out.log",
+    log_file: "logs/combined.log",
+    time: true
+  }]
+};
