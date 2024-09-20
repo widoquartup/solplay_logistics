4c8c0a08fdf66cae2132d2743c76beebe2eb3c6b
Guillermo Domínguez <widomin@hotmail.com>
2024-09-20 10:30:44 +0200

    cambios a uri para conexión mongo

--- backend/src/base/mongoConnection.ts
+++ backend/src/base/mongoConnection.ts
@@ -9,23 +9,23 @@ export const connectToMongoDB = async () => {
         return;
     }
 
-    const username = encodeURIComponent(process.env.MONGO_USERNAME ?? '');
-    const password = encodeURIComponent(process.env.MONGO_PASSWORD ?? '');
-    const clusterUrl = process.env.MONGO_CLUSTER;
-    const authSource = "admin";
+    // const username = encodeURIComponent(process.env.MONGO_USERNAME ?? '');
+    // const password = encodeURIComponent(process.env.MONGO_PASSWORD ?? '');
+    // const clusterUrl = process.env.MONGO_CLUSTER;
+    // const authSource = "admin";
     const database = process.env.MONGO_DB ?? 'defaultDB';
-    let proto = 'mongodb';
+    // let proto = 'mongodb';
 
-    if (process.env.MONGO_IS_SRV === 'yes') {
-        proto = 'mongodb+srv';
-    }
+    // if (process.env.MONGO_IS_SRV === 'yes') {
+    //     proto = 'mongodb+srv';
+    // }
 
-    const uri = `${proto}://${username}:${password}@${clusterUrl}/${database}?authSource=${authSource}`;
-    
+    // const uri = `${proto}://${username}:${password}@${clusterUrl}/${database}?authSource=${authSource}`;
+    const uri = process.env.DB_URI ?? '';
     try {
         await mongoose.connect(uri);
-        console.log('Conectado a DB:');
-        // console.log('Conectado a DB:', uri, database);
+        // console.log('Conectado a DB:');
+        console.log('Conectado a DB:', uri, database);
     } catch (err) {
         console.error('Error al conectar a MongoDB:', err);
     }
